9214166b88ef51e4eece61cdaf4bcc26a70bb91b
Richard Levey <richard.levey@4g-capital.com>
2020-05-22 10:09:12 +0200

    Code clean up on things we wont be using moving forward

--- dogs/app/src/main/java/com/example/dogs/view/DetailFragment.kt
+++ dogs/app/src/main/java/com/example/dogs/view/DetailFragment.kt
@@ -27,13 +27,6 @@ class DetailFragment : Fragment() {
 
         arguments?.let {
             dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
-            textView2.text = dogUuid.toString()
-        }
-
-        buttonList.setOnClickListener{
-            val action = DetailFragmentDirections.actionListFragment()
-
-            Navigation.findNavController(it).navigate(action)
         }
     }
 

6512c0b4be811f48278d276a3e09e4a0c169214b
Richard Levey <richard.levey@4g-capital.com>
2020-05-22 09:16:04 +0200

    Navigagion view and fragments set up

--- dogs/app/src/main/java/com/example/dogs/view/MainActivity.kt
+++ dogs/app/src/main/java/com/example/dogs/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dogs
+package com.example.dogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dogs.R
 
 class MainActivity : AppCompatActivity() {
 

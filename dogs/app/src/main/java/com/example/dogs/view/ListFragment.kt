006755afcc4003e6ac54e5c0acca28baabe7581b
Richard Levey <richard.levey@4g-capital.com>
2020-05-22 09:41:40 +0200

    Navigation functional

--- dogs/app/src/main/java/com/example/dogs/view/ListFragment.kt
+++ dogs/app/src/main/java/com/example/dogs/view/ListFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.example.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -16,6 +18,15 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonDetail.setOnClickListener{
+            val action = ListFragmentDirections.actionDetailFragment()
+
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 }

daa121732826243e2d326a25ef864aa0d546d1af
Richard Levey <richard.levey@4g-capital.com>
2020-06-01 10:20:58 +0200

    Set up retrofit

--- dogs/app/src/main/java/com/example/dogs/model/Model.kt
+++ dogs/app/src/main/java/com/example/dogs/model/Model.kt
@@ -1,11 +1,26 @@
 package com.example.dogs.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DogBreed (
+    @SerializedName("id")
     val breedId: String?,
+
+    @SerializedName("name")
     val dogBreed: String?,
+
+    @SerializedName("life_sapn")
     val lifespan: String?,
+
+    @SerializedName("breed_group")
     val breedGroup: String?,
+
+    @SerializedName("bred_for")
     val bredFor: String?,
+
+    @SerializedName("temperament")
     val temperament: String?,
+
+    @SerializedName("url")
     val imageUrl: String?
 )

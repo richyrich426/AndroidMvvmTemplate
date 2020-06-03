d193a6d6354ab7da0e2b62a95e17069df8e0106f
Richard Levey <richard.levey@4g-capital.com>
2020-06-03 09:52:06 +0200

    Retrofit and rxjava implemented

--- dogs/app/src/main/java/com/example/dogs/model/Model.kt
+++ dogs/app/src/main/java/com/example/dogs/model/Model.kt
@@ -9,7 +9,7 @@ data class DogBreed (
     @SerializedName("name")
     val dogBreed: String?,
 
-    @SerializedName("life_sapn")
+    @SerializedName("life_span")
     val lifespan: String?,
 
     @SerializedName("breed_group")

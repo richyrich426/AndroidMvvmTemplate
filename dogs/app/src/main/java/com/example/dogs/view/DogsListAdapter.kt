5c02ddd62ef494b5cf0cff7e12b07d0cc8cf73a4
Richard Levey <richard.levey@4g-capital.com>
2020-06-03 10:15:39 +0200

    Using glide to load images

--- dogs/app/src/main/java/com/example/dogs/view/DogsListAdapter.kt
+++ dogs/app/src/main/java/com/example/dogs/view/DogsListAdapter.kt
@@ -7,6 +7,8 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dogs.R
 import com.example.dogs.model.DogBreed
+import com.example.dogs.util.getProgressDrawable
+import com.example.dogs.util.loadImage
 import kotlinx.android.synthetic.main.item_dog.view.*
 
 class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
@@ -32,6 +34,7 @@ class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<D
         holder.view.setOnClickListener{
             Navigation.findNavController(it).navigate(ListFragmentDirections.actionDetailFragment())
         }
+        holder.view.imageView.loadImage(dogsList[position].imageUrl, getProgressDrawable(holder.view.imageView.context))
     }
 
     class DogViewHolder(var view: View): RecyclerView.ViewHolder(view)

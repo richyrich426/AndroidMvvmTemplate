d47c2aebe13cba1c1cb7bac04e9afd241b32fcf0
Richard Levey <richard.levey@4g-capital.com>
2020-07-19 20:00:38 +0200

    Detail view, database done for simple retrieval

--- dogs/app/src/main/java/com/example/dogs/viewmodel/DetailViewModel.kt
+++ dogs/app/src/main/java/com/example/dogs/viewmodel/DetailViewModel.kt
@@ -1,15 +1,19 @@
 package com.example.dogs.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.dogs.model.DogBreed
+import com.example.dogs.model.DogDatabase
+import kotlinx.coroutines.launch
 
-class DetailViewModel: ViewModel() {
+class DetailViewModel(application: Application): BaseViewModel(application) {
 
     val dogLiveData = MutableLiveData<DogBreed>()
 
-    fun fetch() {
-        val dog = DogBreed("1", "Corgi", "15 years", "Breed Group", "Bred for", "Temperament", "")
-        dogLiveData.value = dog
+    fun fetch(uuid: Int) {
+        launch {
+            val dog = DogDatabase(getApplication()).dogDao().getDog(uuid)
+            dogLiveData.value = dog
+        }
     }
 }
